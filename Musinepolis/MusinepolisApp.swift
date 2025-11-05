0423267e779f259de89312ae23a8ef56a7f48ad0
Chemi <josemiguel.guerrerojim@gmail.com>
2025-11-05 17:33:16 +0100

    Initial Commit

---
+++ Musinepolis/MusinepolisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusinepolisApp.swift
+//  Musinepolis
+//
+//  Created by José Miguel Guerrero Jiménez on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MusinepolisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
